e5e8bcd7cfb8db1b6253a0136c0194cce07ee207
lucamelis <lm.lucamelis@gmail.com>
2015-07-08 14:33:43 +0100

    changes

--- share_script.py
+++ share_script.py
@@ -1,101 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-
-import numpy as np
-import scipy as sp
-import pandas as pd
-
-import datetime as dt
-import re
-from collections import Counter
-from itertools import combinations
-from sklearn import ensemble, feature_extraction, preprocessing, cross_validation, metrics
-from sklearn.decomposition import TruncatedSVD
-
-import operator
-
-def getPrediction(pred, true):
-    test_size = float(true.shape[0])
-    d = {}
-    d["tp"] = sum( (pred == 1) & (true==1) )/test_size
-    d["fp"] = sum( (pred == 1) & (true==0) )/test_size
-    d["tn"] = sum( (pred == 0) & (true==0) )/test_size
-    d["fn"] = sum( (pred == 0) & (true==1) )/test_size
-    d["support"] = test_size
-
-    return d
-
-def cleanData(data):
-
-    data = data.fillna("0")
-
-    # Remove port numbers that do not exist
-    data = data[data.src_prt <= 65535]
-    data = data[data.src_prt != 0]    
-
-    data = data[data.target_prt <= 65535]
-    data = data[data.target_prt != 0]    
-    
-    # Remove IP addresses that do not make sense as adversaries (0, 10, 127, 192.168, 169.254, 172.16)
-    ip_private_6 = ["169.254", "192.168","172.016", "172.017","172.018",
-                    "172.019","172.020","172.021","172.022","172.023",
-                    "172.024","172.025","172.026","172.027","172.028","172.029","172.030","172.031"]
-    ip_private = ["010", "000", "127","224","225","226","227","228",
-                  "229","230","231","232","233","234","235","236",
-                  "237","238","239","240","241","242","243","244",
-                  "245","246","247","248","249","250","251","252","253","254","255"]
-
-    ip_pattern = "^\d{3}.\d{3}.\d{3}.\d{3}$"
-    criterion = data['src_ip'].map(lambda x: re.match(ip_pattern,x) is not None )  
-    data = data[criterion]
-
-    criterion = data['src_ip'].map(lambda x: np.logical_not( (x[0:7] in ip_private_6) )  )
-    data = data[criterion]
-
-    criterion = data['src_ip'].map(lambda x: np.logical_not( (x[0:3] in ip_private) )  )
-    data = data[criterion]
-
-    return data
-
-def timeToSeconds(date):
-    timesince = dt.datetime(*date) - dt.datetime(*date[0:3])
-    return int(timesince.total_seconds())
-
-encoder = feature_extraction.DictVectorizer()
-labeller = preprocessing.LabelEncoder()
-
-
-#time window
-day = dt.datetime(2015,06,06)
-num_tests = 1
-train_window = 6
-
-names = np.array(["ID","Y","M","D","hh","mm","ss","src_ip","src_prt","target_prt","prot","flag","target_ip"])
-col_idx =[3,7,8,9,12]
-#training features
-# categorical = ["target_ip","flag", "prot", "src_prt","target_prt"]
-
-#targets labels
-labels = ["label"]
-
-data_dir = "data/"
-parser_params = { 
-            "nrows": 2000000, 
-            "usecols": col_idx, #range(1,len(names)+1), 
-            "names": names[col_idx], 
-            "sep": '\t|-|:', 
-            "engine": 'python'
-            }
-
-
-forest_params = { 
-            'max_depth' : None,
-            'min_samples_split' : 2,
-            'criterion': 'entropy',
-            'n_estimators' : 100, 
-            'n_jobs' : -1,
-            }
+from util import *
 
 do_feat_extraction = False
 
@@ -126,8 +32,12 @@ for i in range(0,num_tests):
     first_day, last_day = np.min(days), np.max(days)
 
     #targetes who are both in train and test set
-    GUB = set(df_logs[df_logs.D < last_day]["target_ip"]) & set(df_logs[df_logs.D == last_day]["target_ip"])
-    criterion = df_logs['target_ip'].map(lambda x: x in GUB)
+    # GUB = set(df_logs[df_logs.D < last_day]["src_ip"]) & set(df_logs[df_logs.D == last_day]["src_ip"])
+    # criterion = df_logs['src_ip'].map(lambda x: x in GUB)
+    # df_logs = df_logs[criterion]
+    
+    GUB_targets = set(df_logs[df_logs.D < last_day]["target_ip"]) & set(df_logs[df_logs.D == last_day]["target_ip"])
+    criterion = df_logs['target_ip'].map(lambda x: x in GUB_targets)
     df_logs = df_logs[criterion]
 
     top_targets = [ k for k,v in Counter( df_logs[df_logs.D < last_day]["target_ip"].to_dense() ).most_common(100) ]

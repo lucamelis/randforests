a5c587550f997a1094d10b4ef35e5c75c175d92f
Apostolos Pyrgelis <lotm@dhcp-26-4.xlate.eduroam.ucl.ac.uk>
2015-10-02 12:20:53 +0100

    set 40 percentile as distance threshold

--- knn_cluster_bl.py
+++ knn_cluster_bl.py
@@ -11,13 +11,15 @@ import pandas as pd
 from itertools import combinations
 from itertools import product
 
+from scipy.sparse import lil_matrix
+
 from sklearn.neighbors import NearestNeighbors
 
 from scipy.stats import itemfreq
 
 kNN_alg = ['auto', 'ball_tree', 'kd_tree', 'brute']
-nn_orgs = [2, 4, 5, 10, 20]
-nn_ips = 5
+nn_orgs = [2, 4, 8, 10, 15]
+nn_ips = 50
 
 stats_list = []
 
@@ -27,11 +29,11 @@ for i in range(0, num_tests):
     start_day = logs_start_day + dt.timedelta(days=i)
 
     # load the window data into a dataframe
-    window_logs = pd.read_pickle(data_dir + "sample.pkl")
-    #window_logs = pd.read_pickle(data_dir + "df_" + start_day.date().isoformat() + ".pkl")
+    #window_logs = pd.read_pickle(data_dir + "sample.pkl")
+    window_logs = pd.read_pickle(data_dir + "df_" + start_day.date().isoformat() + ".pkl")
 
     #extract /24 subnets from IPs # TODO: we should play around with /16, /8 as well
-    window_logs.src_ip = window_logs.src_ip.map(lambda x: x[:7])
+    window_logs.src_ip = window_logs.src_ip.map(lambda x: x[:11])
 
     # get the contributors of the window logs
     top_targets = np.unique( window_logs["target_ip"] )
@@ -48,6 +50,8 @@ for i in range(0, num_tests):
     print 'Training set size: ', train_set.shape[0]
     print 'Test set size: ', test_set.shape[0]
 
+    del window_logs
+    
     # get the pairs between organizations 
     target_pairs = combinations(top_targets, 2)
 
@@ -56,7 +60,7 @@ for i in range(0, num_tests):
     reverse_ind_orgs = dict( zip(ind_orgs.values(), ind_orgs.keys()) )
 
     # organization to organization matrix
-    o2o = np.zeros((top_targets.size, top_targets.size, days.size))
+    o2o = np.zeros((top_targets.size, top_targets.size, len(date_list)))
 
     # create a dictionary where each contributors stores his attacker set over all the training window
     print 'creating attacker set dictionary...'
@@ -69,15 +73,13 @@ for i in range(0, num_tests):
     victim_daily_set = dict()
     for target in top_targets:
         victim_daily_set[target] = dict()
-        for idx, day in enumerate(days):
+        for idx, day in enumerate(date_list):
             victim_daily_set[target][idx] = set( train_set[ (train_set.target_ip == target) & (train_set.D == day) ].src_ip)     
     
     # create the organisation to organisation matrix with 
-    # TODO: compute some other form of similarity e.g. Jaccard, Cosine
-
     print 'Creating o2o matrix...'
     for pair in target_pairs:
-        for idx, day in enumerate(days):
+        for idx, day in enumerate(date_list):
             o2o[ ind_orgs[pair[0]], ind_orgs[pair[1]], idx] = len( victim_daily_set[pair[0]][idx] & victim_daily_set[pair[1]][idx])
             o2o[ ind_orgs[pair[1]], ind_orgs[pair[0]], idx] = o2o[ind_orgs[pair[0]], ind_orgs[pair[1]], idx]
                     
@@ -93,13 +95,17 @@ for i in range(0, num_tests):
         
         # compute nearest neighbors based on the ip2ip matrix
         neighbors = NearestNeighbors(n_neighbors = k, algorithm = kNN_alg[1]).fit( o2o.sum(axis=2) )
-        dist, indices = neighbors.kneighbors(o2o.sum(axis=2))
-        print dist
+        distances, indices = neighbors.kneighbors(o2o.sum(axis=2))
 
+        # pick 40 % percentile of distances
+        distance_threshold = np.percentile(distances, 40)
+        print 'Distance threshold is :', distance_threshold
+        
         # dictionary storing for each contributor a list with its nearest neighbors
         org_neighbors = dict()
         for idx, x in enumerate(indices):
-            org_neighbors[reverse_ind_orgs[idx]] = [reverse_ind_orgs[y] for y in x]
+            org_neighbors[reverse_ind_orgs[idx]] = [reverse_ind_orgs[y] for idy, y in enumerate(x) if distances[idx][idy] <= distance_threshold]
+            #org_neighbors[reverse_ind_orgs[idx]] = [reverse_ind_orgs[y] for y in x ]
 
         # global blacklist - this dictionary holds each contributor's global blacklist (i.e. the one generated from his cluster)
         gub_blacklists = dict()
@@ -111,6 +117,9 @@ for i in range(0, num_tests):
         # ip2ip corelation blacklist
         ip2ip_blacklists = dict()    
     
+        # combined ip2ip and intersection blacklist
+        int_ip2ip_blacklists = dict()
+    
         # what happens in the cluster stays in the cluster     
         for contributor in org_neighbors:
            
@@ -121,27 +130,35 @@ for i in range(0, num_tests):
             criterion = train_set.target_ip.map(lambda x: x in c_contributors)
             logs = train_set[criterion].copy()
 
-            attackers = np.unique(logs["src_ip"])
-
-            ind_ips = dict( zip(attackers, range(attackers.size) ) )
+            top_attackers = getHeavyHitters( logs["src_ip"] ,0.9 )
+            
+            # limit the attacker to top1000
+            top_attackers = top_attackers[:1000]
+            print 'Top attacks : ', top_attackers.size            
+            
+            ind_ips = dict( zip(top_attackers, range(top_attackers.size) ) )
             reverse_ind_ips = dict( zip(ind_ips.values(), ind_ips.keys()) )
 
+            criterion = logs.src_ip.map(lambda x: x in top_attackers)
+            logs = logs[criterion]
             logs.src_ip = logs.src_ip.map(lambda x : ind_ips[x])
 
-            df_gr = logs.groupby("D").apply(lambda x: np.bincount( x["src_ip"], minlength=attackers.size) )
-
-            ip2ip = np.zeros( attackers.size**2 )
+            df_gr = logs.groupby("D").apply(lambda x: np.bincount( x["src_ip"], minlength=top_attackers.size) )
+            
+            ip2ip = lil_matrix(np.zeros( top_attackers.size**2, dtype=np.uint32))
 
             print 'computing ip2ip matrix...'
-            for w, v in df_gr.iteritems():
-                ip2ip += [min(f) for f in product(v,v)]
-
-            ip2ip = ip2ip.reshape(attackers.size, -1)
-
+            for l, v in df_gr.iteritems():
+                ip2ip += np.array([np.uint32(min(f)) for f in product(v,v)])
+            
+            del df_gr
+            
             # compute nearest neighbors based on the ip2ip matrix
-            nbrs = NearestNeighbors(n_neighbors = nn_ips, algorithm = kNN_alg[0]).fit( ip2ip )
-            _, indic = nbrs.kneighbors(ip2ip)
+            nbrs = NearestNeighbors(n_neighbors= min(nn_ips, top_attackers.size), algorithm= kNN_alg[1]).fit( ip2ip.toarray().reshape(top_attackers.size, top_attackers.size) )
+            _, indic = nbrs.kneighbors(ip2ip.toarray().reshape(top_attackers.size, top_attackers.size))
 
+            del ip2ip; del top_attackers
+            
             # for each attacker ip store the k corelated ips
             corelated_ips = dict()
 
@@ -149,44 +166,50 @@ for i in range(0, num_tests):
                 corelated_ips[reverse_ind_ips[idx]] = [reverse_ind_ips[y] for y in x]
     
             # compute gub blacklist
-            #print 'computing gub blacklist...'        
             gub = set()
             gub = gub_prediction(c_contributors, l_blacklists)        
             gub_blacklists[contributor] = gub
+            del gub
             
             # compute intersection blacklists
-            #print 'computing intersection blacklist...'
             int_set = set()
             int_set = intersection_prediction(contributor, c_contributors, l_blacklists, victim_set)
             int_blacklists[contributor] = int_set
+            del int_set
                                                             
             # make ip2ip corelation prediction
             ip2ip_set = set()
             ip2ip_set = ip2ip_prediction(contributor, l_blacklists, corelated_ips)
             ip2ip_blacklists[contributor] = ip2ip_set
+            del ip2ip_set
             
-            #del corelated_ips; del df_gr; del ip2ip; del attackers; 
+            # make combined ip2ip and intersection prediction
+            int_ip2ip_set = set()
+            int_ip2ip_set = combined_int_ip2ip_prediction(int_blacklists[contributor], ip2ip_blacklists[contributor])
+            int_ip2ip_blacklists[contributor] = int_ip2ip_set
+            del int_ip2ip_set
+            
+            del corelated_ips;
             
         # predictions verification part
         for target in top_targets:
-    
-            stats = verify_prediction(l_blacklists[target], gub_blacklists[target], int_blacklists[target], ip2ip_blacklists[target], set( test_set[ (test_set.target_ip == target) ].src_ip ) )
 
-            #stats = verify_prediction(l_blacklists[target], gub_blacklists[target], int_blacklists[target], set( test_set[ (test_set.target_ip == target) ].src_ip ) )
-            
+            stats = verify_prediction(l_blacklists[target], gub_blacklists[target], int_blacklists[target], ip2ip_blacklists[target], int_ip2ip_blacklists[target], set( test_set[ (test_set.target_ip == target) ].src_ip ) )
+                        
             stats["D"] = last_day
             stats["n_clusters"] = k
             stats["target"] = target
 
             stats_list.append(stats)    
-
-        #del train_set; del test_set; del window_logs
-        #del l_blacklists; del gub_blacklists; del int_blacklists
+        
+        del gub_blacklists; del int_blacklists; del ip2ip_blacklists; del int_ip2ip_blacklists
+        
+    del train_set; del test_set; del l_blacklists        
 
 df_stats = pd.DataFrame(stats_list)
 
 # print out some stats
-compute_stats(df_stats)
+compute_stats('knn', df_stats)
 
 # save the df for later processing
 df_stats.to_pickle("k_nn_stats.pkl")
